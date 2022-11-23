c070c26cbe5187167cdeb6717803093fec1697a8
Elroy Kanye <elroykanye@gmail.com>
2022-11-23 08:28:02 +0100

    initial commit

---
+++ ex01_hello_rust/src/main.rs
@@ -0,0 +1,8 @@
+fn main () { // a function called main
+    println!("Hello, I am Elroy Kanye and I am {} years old", 98);
+    /* 
+    println!("{}", 20) 
+    oqsodio
+    explanation
+    */ 
+}
