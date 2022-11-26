5ad424577eb707e697059dc8079274b6e0fc5b8d
Elroy Kanye <elroykanye@gmail.com>
2022-11-26 08:46:40 +0100

    playing with variables

---
+++ ex06_playing_with_variables/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut age = 0;
+
+    println!("{}", age);
+
+    age += 1; // age = age + 1; // age++
+
+    println!("{} first", age);
+
+    age -= 1; // age = age - 1
+
+
+    age *= 1; //  age = age * 1
+    age /= 2; // age  = age / 2
+
+    println!("{} second", age);
+}
+
+// age -> i32
