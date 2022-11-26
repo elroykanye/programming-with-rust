e4933b8ee67d0755e5ebdb03a2354bdb07511265
Elroy Kanye <elroykanye@gmail.com>
2022-11-26 07:12:52 +0100

    variables and values

---
+++ ex04_variables_and_values/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let age = 98; // interger 32
+    let is_male = true; // boolean
+    let name = "Elroy Kanye"; // string
+    
+    println!("age = {}", age);
+    println!("isMale = {}", is_male);
+    println!("name = {}", name);
+}
+
+// isMale, dangerZone, lastName -> camel case
+// is_male, danger_zone, last_name -> snake case
+// value - 98, "Elroy Kanye", true
+// age <- 98, name <- "Elroy"
