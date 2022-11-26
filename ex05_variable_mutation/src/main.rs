3f5872e24156fee9b05d6236031e62978e7c62af
Elroy Kanye <elroykanye@gmail.com>
2022-11-26 07:31:11 +0100

    variable mutation

---
+++ ex05_variable_mutation/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut age = 98;
+    let height: f32 = 174.54;
+    let is_male = true;
+    let name = "Elroy Kanye"; 
+
+    
+    println!("old age = {}", age);
+    println!("height = {}", height);
+    println!("isMale = {}", is_male);
+    println!("name = {}", name);
+
+    
+    age = 214;
+    println!("new age  = {}", age);
+}
