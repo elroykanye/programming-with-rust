0937c5de26b8be2b715e09c1975769153ff417ed
Elroy Kanye <elroykanye@gmail.com>
2022-11-30 19:39:13 +0100

    variable scopes

---
+++ ex10_variable_scopes/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut i = 0;
+
+    while i <= 10 {
+        println!("{}", i);
+
+        let mut k = i * 2;
+        println!("{}\n", k);
+
+        i += 1;
+    }
+
+    i = 9020292;
+    println!("{}", i)
+}
