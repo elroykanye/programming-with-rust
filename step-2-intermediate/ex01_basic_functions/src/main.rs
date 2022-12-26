3c0cf96b06d7377ef43a0b167f883ac8b98b0d3a
Elroy Kanye <elroykanye@gmail.com>
2022-12-26 17:26:45 +0100

    mend

--- step-2-intermediate/ex01_basic_functions/src/main.rs
+++ step-2-intermediate/ex01_basic_functions/src/main.rs
@@ -1,21 +1,29 @@
-fn example_function_2() {
-    for i in 1..10 {
-        print!("{}", i);
-    }
+fn print_a_greeting() {
+    println!("Hello, good afternoon.");
+
+    let a = 3;
+    print!("{}", a);
 }
 
 fn main() {
     println!("Hello, world!");
 
-    example_function();
-    prints_a_greeting();
+    example_function_1();
+    print_a_greeting();
+    print_a_name();
+
+    fn print_a_name() {
+        println!("Elroy Kanye");
+    }
 }
 
-fn example_function() {
+fn example_function_1() {
     println!("Hello example function");
     example_function_2();
 }
 
-fn prints_a_greeting() {
-    println!("Hello, good afternoon.");
+fn example_function_2() {
+    for i in 1..11 {
+        print!("{} ", i);
+    }
 }
